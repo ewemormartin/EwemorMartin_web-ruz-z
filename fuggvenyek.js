059e8ba3ca80b96f4f4878c4df4cb199e6e02a55
ewemormartin <martinewemor@gmail.com>
2024-04-18 12:03:19 +0200

    alapFuggvenyek

---
+++ fuggvenyek.js
@@ -0,0 +1,21 @@
+    /* <div class="container mt-3">
+            <div class="card" style="width:400px">
+                <img class="card-img-top" src="kepek/" alt="Card image" style="width:100%">
+                <div class="card-body">
+                    <h4 class="card-title">Termék neve</h4>
+                    <p class="card-text">Lerias</p>
+                    <a href="#" class="btn btn-primary">See Profile</a>
+                </div>
+                
+            </div>
+        </div>
+    */
+export function listaLetrehoz() {
+    let txt = ""
+}
+
+
+export function listaMegjelenites(ADATLISTA){
+    
+
+}
