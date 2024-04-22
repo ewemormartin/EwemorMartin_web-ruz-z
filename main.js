0c82a647f300ea4a98cfb278cc4f7ce9af074caf
Ewemor Martin <martinewemor@gmail.com>
2024-04-22 17:37:59 +0200

    merge fix

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { ADATLISTA } from "./adatok.js";
 import { listaLetrehoz, listaMegjelenites } from "./fuggvenyek.js";
-import { Kosárba } from "./fuggvenyek.js";
+import { Kosarba } from "./fuggvenyek.js";
 
 init();
 
@@ -8,6 +8,7 @@ init();
 
 function init() {
     listaMegjelenites(listaLetrehoz(ADATLISTA));
-    Kosárba();  
+    Kosarba();
+      
     
 }
